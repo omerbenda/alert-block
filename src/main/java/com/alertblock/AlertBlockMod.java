22de896e01b9d80c91b622f2fea600edae572de6
omerbenda <34948058+omerbenda@users.noreply.github.com>
2023-10-28 18:58:20 +0300

    feat(AlertBlockMod.java): Add mod files for minecraft 1.19.2

---
+++ src/main/java/com/alertblock/AlertBlockMod.java
@@ -0,0 +1,43 @@
+package com.alertblock;
+
+import com.alertblock.block.ModBlocks;
+import com.alertblock.blockentity.ModBlockEntities;
+import com.alertblock.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(AlertBlockMod.MOD_ID)
+public class AlertBlockMod {
+    public static final String MOD_ID = "alertblock";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public AlertBlockMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+        ModBlockEntities.register(modEventBus);
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+//        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
